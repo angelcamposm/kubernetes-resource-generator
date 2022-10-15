4f0db90da06d5c8ed028b381818f1e85c42ebd33
Angel <angel.campos.m@outlook.com>
2022-10-15 11:19:57 +0200

    refactor: simplify toYaml() method

--- src/Traits/Exportable.php
+++ src/Traits/Exportable.php
@@ -13,12 +13,49 @@ trait Exportable
      */
     public function toYaml(): string
     {
-        $yaml = Yaml::dump($this->toArray());
+        $resource = $this->toArray();
 
-        $searchString = ['/-\n\s+/', "/'\[/", "/]'/"];
+        //TODO: Add option to control if must be sorted by key.
+        ksort($resource);
 
-        $replaceString = ['- ', '[', ']'];
+        $yaml = Yaml::dump($resource);
+
+        $yaml = self::replaceCaretWithSingleQuote($yaml);
+
+        $yaml = self::fixListItemsInNewLine($yaml);
+
+        $searchString = ["/'\[/", "/]'/"];
+
+        $replaceString = ['[', ']'];
 
         return preg_replace($searchString, $replaceString, $yaml);
+    }
+
+    /**
+     * With some definition options, it's necessary to display between
+     * single quotes.
+     * A caret is inserted in the value assignment of the option, then
+     * we replace caret with single quotes.
+     *
+     * @param string $yaml
+     *
+     * @return string
+     */
+    private static function replaceCaretWithSingleQuote(string $yaml): string
+    {
+        return str_replace('^', "'", $yaml);
+    }
+
+    /**
+     * With Symfony::Yaml when an array is converted to yaml list, a new line
+     * is inserted after a dash symbol.
+     *
+     * @param string $yaml
+     *
+     * @return string
+     */
+    private static function fixListItemsInNewLine(string $yaml): string
+    {
+        return preg_replace('/-\n\s+/', '- ', $yaml);
     }
 }
