e1fabfee822fecc992b36a955e24c19f3d7e4c47
Angel <angel.campos.m@outlook.com>
2022-10-15 13:27:53 +0200

    feat: add addContainers(), addImagePullSecret(), addImagePullSecrets() and AddNodeSelectors() methods to Pod.php class

--- src/Resources/Pod.php
+++ src/Resources/Pod.php
@@ -29,6 +29,52 @@ final class Pod extends K8sWorkloadResource
         parent::__construct(kind: 'Pod', apiVersion: 'v1');
     }
 
+    public function addContainers(array $containers): Pod
+    {
+        foreach($containers as $key => $value) {
+            $this->containers[$key] = $value;
+        }
+        return $this;
+    }
+
+    /**
+     * Allow a Pod to use a Secret to pull an image from a private container
+     * image registry or repository.
+     *
+     * @param string $secretName
+     *
+     * @return $this
+     */
+    public function addImagePullSecret(string $secretName): Pod
+    {
+        $this->imagePullSecrets[] = ['name' => $secretName];
+        return $this;
+    }
+
+    /**
+     * Allow a Pod to use a Secret to pull an image from a private container
+     * image registry or repository.
+     *
+     * @param string $secretName
+     *
+     * @return $this
+     */
+    public function addImagePullSecrets(array $secrets): Pod
+    {
+        foreach ($secrets as $key => $secret) {
+            $this->imagePullSecrets[] = ['name' => $secret];
+        }
+        return $this;
+    }
+
+    public function addNodeSelectors(array $selectors): Pod
+    {
+        foreach ($selectors as $key => $value) {
+            $this->nodeSelectors[$key] = $value;
+        }
+        return $this;
+    }
+
     /**
      * Set the Operating System name.
      * It can be one of linux|windows.
